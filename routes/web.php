2d893585dfae37d5bc4ea67e995d31c977797e26
sabian77 <sabianrakasinau@gmail.com>
2025-07-05 15:40:02 +0700

    membuat template ui dari tailwind plus

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('dashboard', function () {
+    return view('home');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
